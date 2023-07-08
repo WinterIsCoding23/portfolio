8f520d70ccaa9cd36d1c992df787460f8bd364c5
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-08 17:47:14 +0200

    exchange logo in html & add subtitle underneath logo in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,10 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Markus{" "}
+            <span className="sm:block hidden">| Full Stack Developer</span>
+          </p>
         </Link>
       </div>
     </nav>
