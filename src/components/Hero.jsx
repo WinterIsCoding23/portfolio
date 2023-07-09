9ba97ef4cd2b81868324d26815f309fbc69f104c
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-09 17:59:19 +0200

    add imports & create violet ball and line with gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingY} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          {/* violet ball */}
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          {/* vertical violet line with a gradient */}
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
