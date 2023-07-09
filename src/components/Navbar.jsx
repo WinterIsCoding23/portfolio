de503ce64f5a95b7cf437dd82d058cfe901825ca
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-09 14:33:50 +0200

    add no-break space in title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,11 +23,12 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Markus{" "}
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Markus &nbsp;
             <span className="sm:block hidden">| Full Stack Developer</span>
           </p>
         </Link>
+
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
             <li
