e754c6204d19f2814fe7ec61cce97fa8cf09a2fd
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-09 19:09:46 +0200

    create Canvas with Computer-model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,40 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return <div>Computers</div>;
+  // 3D-models saved in GTLF-file-format
+  const computer = useGLTF("./desktop_pc/scene.gtlf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* CanvasLoader to show while model is loading:  */}
+      <Suspense fallback={<CanvasLoader />}>
+        {/* OrbitCOntrols to move model to the left and to the right: */}
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default Computers;
