cd43c032028d68b03302ff28bb26cf4951780399
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-09 18:31:06 +0200

    add text & render ComputersCanvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,18 @@ const Hero = () => {
           {/* vertical violet line with a gradient */}
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Markus</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop websites, <br className="sm:block hidden" />
+            Javascript-applications and interfaces
+          </p>
+        </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
