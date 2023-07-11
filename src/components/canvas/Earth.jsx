169e531bae255c9aeff90b0a39594f14d0c828ae
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 18:09:42 +0200

    create earth-model but not showing

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,11 +1,43 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  // useGLTF-hook:
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return <primitive object={earth.scene} />;
 };
 
-export default Earth;
+const EarthCanvas = () => {
+  <Canvas
+    shadows
+    frameloop="demand"
+    dpr={[1, 2]}
+    gl={{ preserveDrawingBuffer: true }}
+    camera={{
+      // fov: field of view
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6],
+    }}
+  >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls
+        autoRotate
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+
+      <Earth />
+
+      <Preload all />
+    </Suspense>
+  </Canvas>;
+};
+
+export default EarthCanvas;
