17bd18a6da86e21875736acec9c8fdab429771b3
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 16:02:06 +0200

    add comments

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -26,12 +26,15 @@ const ProjectCard = ({
         }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
+        {/* add the project-image */}
         <div className="relative w-full h-[230px]">
           <img
             src={image}
             alt="project_image"
             className="w-full h-full object-cover rounded-2xl"
           />
+
+          {/* add the Github-logo with link (...to the project) */}
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
@@ -44,6 +47,24 @@ const ProjectCard = ({
               />
             </div>
           </div>
+        </div>
+
+        {/* add project-name & -description */}
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        {/* add hashtags for the tech-stack used in the respective project */}
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
         </div>
       </Tilt>
     </motion.div>
