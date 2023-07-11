33e809f97f8c055aec60590ab7065511eeb06811
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 16:15:59 +0200

    add p & h2

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -10,7 +10,12 @@ const Feedbacks = () => {
     <div className={`mt-12 bg-black-100 rounded-[20px]`}>
       <div
         className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
-      ></div>
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
     </div>
   );
 };
