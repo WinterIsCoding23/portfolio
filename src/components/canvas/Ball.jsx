dcd691d60ba241eb14266f7420c7030f101b86bf
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 12:35:38 +0200

    create structure for BallCanvas by copying Canvas from Computers

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,8 +10,34 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return <div>Ball</div>;
+};
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* CanvasLoader to show while model is loading:  */}
+      <Suspense>
+        {/* OrbitCOntrols to move model to the left and to the right: */}
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default Ball;
