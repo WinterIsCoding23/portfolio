638cb3311de70adf57e820518322dbc6778274b8
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 10:02:00 +0200

    create SectionWrapper in new folder hoc

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,9 @@
+// rafce: extension shortcut for a React component (with the name of the file)
+
+import React from "react";
+
+const SectionWrapper = () => {
+  return <div>SectionWrapper</div>;
+};
+
+export default SectionWrapper;
