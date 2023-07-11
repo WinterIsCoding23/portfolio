fb46bb7b26a8db1032a571917c94d09f4c69eea4
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 14:40:53 +0200

    fix bug inside Float caused by prettier-auto-formatting

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,31 +8,45 @@ import {
   useTexture,
 } from "@react-three/drei";
 
+
+
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
-  return <div>Ball</div>;
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
 };
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
       {/* CanvasLoader to show while model is loading:  */}
       <Suspense>
-        {/* OrbitCOntrols to move model to the left and to the right: */}
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
+        {/* OrbitControls to move model to the left and to the right: */}
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
       </Suspense>
 
       <Preload all />
@@ -40,4 +54,4 @@ const BallCanvas = ({ icon }) => {
   );
 };
 
-export default Ball;
+export default BallCanvas;
