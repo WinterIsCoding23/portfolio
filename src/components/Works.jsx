1fc4c3e5d19cb3dee41d4ee21983218910348283
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 15:43:37 +0200

    add Tilt aka 3 dark moving horizontal bars

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,19 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
-  return <motion.div>Test</motion.div>;
+  return (
+    // variants: projects float in one by one
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      ></Tilt>
+    </motion.div>
+  );
 };
 
 const Works = () => {
