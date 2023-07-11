ee52be44f373a719f39ed8c34b15963adf5ad37c
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-11 14:52:04 +0200

    add motion.div as copied from Experience

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
+  // fragments bc Works will be wrapped in the SectionWrapper (= div)
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Works
+export default SectionWrapper(Works, "");
